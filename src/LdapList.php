785cfb414d8a9347fe4a41768418ae1b5ca5e318
Oliver Payne <opayne@ripe.net>
2019-07-09 15:18:23 +0200

    code tidyup and English improvements

--- src/LdapList.php
+++ src/LdapList.php
@@ -42,7 +42,7 @@ class LdapList
     {
         foreach ($this->items as $key => $item) {
             if (! $item->authenticate($username, $password, $filter)) {
-                unset ($this->items[$key]);
+                unset($this->items[$key]);
                 continue;
             }
             return true;
@@ -65,7 +65,7 @@ class LdapList
         }
 
         if ($allFailed) {
-            throw new AuthLDAP_Exception('No bind successfull');
+            throw new AuthLDAP_Exception('Bind unsuccessful');
         }
 
         return true;
