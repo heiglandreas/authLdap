43a0389d1811618ac7cc7df87d9614384c600dcf
Andreas Heigl <andreas@heigl.org>
2020-07-19 12:54:54 +0200

    Fix Travis

--- .ci/load_fixtures.sh
+++ .ci/load_fixtures.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 FIXTURES_DIR="$DIR/ldif"
 
 load_fixture () {
-  ldapadd -x -H ldap://127.0.0.1:389/ -D "cn=Manager,dc=example,dc=org" -w insecure -f $1
+  ldapadd -x -H ldap://127.0.0.1:389/ -D "cn=admin,dc=example,dc=org" -w insecure -f $1
 }
 
 for FIXTURE in `ls ${FIXTURES_DIR}`
