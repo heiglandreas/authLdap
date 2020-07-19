f025c8cfe83e1efd857e2e647ef89458b6678ec0
Andreas Heigl <andreas@heigl.org>
2020-07-19 09:42:39 +0200

    Update PHP Minimum requirements
    This will set the PHP Minimum requirements to PHP7.2

---
+++ src/LdapUri.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright Andrea Heigl <andreas@heigl.org>
+ *
+ * Licenses under the MIT-license. For details see the included file LICENSE.md
+ */
+
+namespace Org_Heigl\AuthLdap;
+
+
+class LdapUri
+{
+
+}
